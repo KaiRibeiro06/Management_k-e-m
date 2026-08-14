80830b4fe85f04d392c1f836d384cf01065f88f6
KaiRibeiro06 <kailaniribeirodemiranda@gmail.com>
2026-08-13 21:27:12 -0300

    estoque

--- src/pages/CadastrarProdutos.jsx
+++ src/pages/CadastrarProdutos.jsx
@@ -40,18 +40,18 @@ function CadastrarProdutos() {
         Cadastrar
       </button>
 
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Cadastro de Produto</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Cadastro de Produto</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               Produto cadastrado com sucesso!
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
               
             </div>
           </div>
--- src/pages/Estoque.jsx
+++ src/pages/Estoque.jsx
@@ -3,11 +3,62 @@ import { Link } from "react-router-dom"
 
 function Estoque() {
     return(
+
         <div className="container mt-5">
             <h1 className="text-center text-rosa">Estoque</h1>
-            <div className="table-responsive mt-4">
-                
-            </div>
+            
+                <div className=" input-group d-flex justify-content-between align-items-center">
+
+                    <span className="input-group-text bg-white border-end-0 border-rosa">
+                        <i className="bi bi-search"></i>
+                    </span>
+
+                    <input className="form-control me-2 border-rosa border-start-0 " type="text" placeholder=  "Buscar produto ou código..."/>
+                    
+                    <button className="btn btn-buscar" type="submit">Buscar</button>
+                </div>
+
+                <div className="produtos border border-rosa border-2 rounded p-4 mt-4">
+
+                    <table className="table table-striped mt-4 border-rosa">
+                        <thead className="bg-rosa text-white">
+                            <tr>
+                                <th scope="col"><button className="btn btn-sm btn-buscar rounded-5">Todos <span>(10)</span></button></th> 
+                                <th scope="col">Produto</th>
+                                <th scope="col">Quantidade</th>
+                                <th scope="col">Imagem</th>
+                            </tr>
+                        </thead>
+                        <tbody> 
+                            <tr>
+                                <td>001</td>
+                                <td>Produto 1</td>
+                                <td>10</td>         
+                                <td>
+                                    <img src="https://via.placeholder.com/50" alt="Produto 1" />
+                                </td>       
+                            </tr>
+                            <tr>
+                                <td>002</td>
+                                <td>Produto 2</td>  
+                                <td>5</td>
+                                <td>
+                                    <img src="https://via.placeholder.com/50" alt="Produto 2" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>003</td>    
+                                <td>Produto 3</td>
+                                <td>8</td>
+                                <td>
+                                <img src="https://via.placeholder.com/50" alt="Produto 3" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+           
         </div>
     )
 }
